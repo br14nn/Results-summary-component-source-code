d6daf1ef18230634e3d0392afdbb8752dd0d0d0b
br14nn <vituallabrian727@gmail.com>
2023-04-14 18:55:01 +0800

    changed some span elem to div

--- src/components/Card/CategoryScore.tsx
+++ src/components/Card/CategoryScore.tsx
@@ -11,11 +11,11 @@ export default function (props: CategoryScoreProps) {
 		<div className={`mb-4 flex flex-row items-center rounded-xl p-4 ${props.bgColor}`}>
 			<img className="mr-2" src={props.icon} alt="Reaction Icon" />
 			<p className={`text-center font-medium ${props.textColor}`}>{props.categoryName}</p>
-			<span className="ml-auto flex flex-row items-center">
+			<div className="ml-auto flex flex-row items-center">
 				<p className="font-bold text-dark-grey-blue">{props.categoryScore}</p>
 				<p className="mx-2 font-bold text-dark-grey-blue/50">/</p>
 				<p className="font-bold text-dark-grey-blue/50">100</p>
-			</span>
+			</div>
 		</div>
 	);
 }
